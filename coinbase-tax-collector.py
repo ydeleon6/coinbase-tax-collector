b5a5161f45301b7284efa9ee7c38fb04c4eeec0b
Yahir De Leon <ydeleon@Annas-Air.askey.com>
2022-12-23 23:24:42 -0600

    Refactor operations on the sale object after loading taxable transactions

--- coinbase-tax-collector.py
+++ coinbase-tax-collector.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 import sys
-import csv
 from argparse import ArgumentError
 from operator import contains
-from cryptocurrency.coinbase import CoinbaseAccount
+from cryptocurrency.coinbase import CoinbaseAccount, TaxableSalesCsvWriter, TotalCapitalGainsTaxCalculator,\
+	 ConsoleOutputWriter, CoinbaseTaxCalculator, formatMoney
 from cryptocurrency.utils import FIFO, LIFO
 
 CSV_OUTPUT_PATH = r'taxable-events.csv'
@@ -21,55 +21,22 @@ if isFifo:
 else:
 	taxMethod = LIFO
 
-def formatMoney(amount):
-	isNegative = amount < 0
-	moneyText = "{:,.2f}".format(abs(amount))
+account = CoinbaseAccount(tax_method=taxMethod)
+account.load_transactions(csvFilePath)
 
-	sign = ""
-	if isNegative:
-		sign = "-"
-	return "{}${}".format(sign, moneyText)
+csvWriter = TaxableSalesCsvWriter(CSV_OUTPUT_PATH, account.sales[0].keys())
+consoleWriter = ConsoleOutputWriter()
+salesCalculator = TotalCapitalGainsTaxCalculator()
 
-def getLossOrGainText(amount):
-	resultAction = "Gains"
-	if amount < 0:
-		resultAction = "Losses"
-	return resultAction
+visitors = [csvWriter, consoleWriter, salesCalculator]
 
-def calculateCoinbaseCapitalGains():
-	account = CoinbaseAccount(tax_method=taxMethod)
-	account.load_transactions(csvFilePath)
+calculator = CoinbaseTaxCalculator(account, visitors)
+calculator.calculate()
 
-	totalGains = 0.0
+csvWriter.shutdown()
 
-	with open(CSV_OUTPUT_PATH, 'w') as outFile:
-		writer = csv.DictWriter(outFile, fieldnames=(account.sales[0].keys()), dialect='unix')
-		writer.writeheader()
-
-		# Add up capital gains.
-		for sale in account.sales:
-			totalGains += sale['Gains']
-			# format money amounts for the console.
-			sale['LossOrGain'] = getLossOrGainText(sale['Gains'])
-			sale['LastPurchasePrice']  = formatMoney(sale['LastPurchasePrice'])
-			sale['SpotPrice']  = formatMoney(sale['SpotPrice'])
-			sale['CostBasis']  = formatMoney(sale['CostBasis'])
-			sale['Total']  = formatMoney(sale['Total'])
-			sale['Gains']  = formatMoney(sale['Gains'])
-			sale['Fees']  = formatMoney(sale['Fees'])
-
-			print()
-			print("Transaction Date: {DateSold}".format(**sale))
-			print("Date {Asset} was last acquired and bought: [{LastAcquired}] {LastPurchasePrice}".format(**sale))
-			print("Cost Basis: {CostBasis} {Currency}".format(**sale))
-			print("Price of {Asset} at Transaction: {SpotPrice} {Currency}".format(**sale))
-			print("You sold {Quantity} of {Asset} for {Total} {Currency} (fees: {Fees}). {LossOrGain} are {Gains} {Currency}".format(**sale))
-			writer.writerow(sale)
-	return totalGains
-
-totalGains = calculateCoinbaseCapitalGains()
 resultAction = "Gains"
-if totalGains < 0:
+if salesCalculator.totalGains < 0:
 	resultAction = "Losses"
 print("")
-print("Total Capital {}: {} USD".format(resultAction, formatMoney(round(totalGains, 2))))
+print("Total Capital {}: {} USD".format(resultAction, formatMoney(round(salesCalculator.totalGains, 2))))
